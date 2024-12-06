6ce119d85e253f786216776bdcdd6ba6d4641839
Abhinav <96286033+AbhinavTheNoobCoder@users.noreply.github.com>
2024-12-06 22:34:17 +0530

    Update Hangman.py
    Added a new update - You can now guess standard English words using a module named wonderwords. Do not worry if you haven't installed it, the code will install it for you when prompted.

--- Hangman.py
+++ Hangman.py
@@ -1,54 +1,160 @@
-import os
-def clear_shell():
-    # Clear the shell based on the operating system
-    os.system('cls' if os.name == 'nt' else 'clear')
+drawings = {
+0: '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''',
 
-print('''Welcome to Hangman.
-Please enter a word which others must guess.
-Minimum length of the word must be 5.
-For every 4 letters in the word, you must reveal one letter.
-''')
+1: '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''',
 
-word_to_guess = input("Enter the word to be guessed by all: ").lower()
+2: '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''',
 
-if len(word_to_guess) < 5: #Minimum length not met
-    raise ValueError("You were supposed to input a word of length more than 5.")
+3: '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''',
 
-revealed_list = []
-guessed_list = []
-for _ in range(len(word_to_guess)//4):
-    reveal_letter = input("Reveal a letter: ").lower()
-    revealed_list.append(reveal_letter)
+4: r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''',
 
-clear_shell()
-hidden_word = ""
-incorrect_tries = 0
-print("Start guessing the word.\n")
-while incorrect_tries < 6:
-    for letter in word_to_guess:
-        if letter in (revealed_list + guessed_list) or (not letter.isalpha()):
-            hidden_word += letter
+5: r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''',
 
-        else:
-            hidden_word += "_"
+6: r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+========='''}
+
+mode = int(input('''Select the mode:
+1 - Standard English words
+2 - Manually enter a word, have the other person guess
+>>> '''))
+
+word_to_guess = "- "
+if mode == 1:
+  import random
+  try:
+    from wonderwords import RandomWord
+    word_generator = RandomWord()
+    word_to_guess = word_generator.word(word_min_length=6)
+    unique_letters = list(dict.fromkeys(word_to_guess, None))
+    # dict does not have duplicate keys, using list we get the keys of the dict
+    # which are the letters of the word
+
+    revealed_letters = random.sample(population=unique_letters, k=len(word_to_guess)//4)
+  
+  except ImportError:
+    choice = int(input('''The "wonderwords" package is not installed.
+Enter 1 to install it and continue with the code.
+Enter any other digit to change the mode to manual.
+>>> '''))
     
-    print(" ".join(hidden_word))
-    if "_" not in hidden_word:
-        print("You won, congratulations.")
-        break
+    if choice == 1:
+      import subprocess, sys
+      try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "wonderwords"])
+        print("wonderwords has been installed successfully!")
 
-    guess = input("Guess a letter of the word: ")
-    if guess not in word_to_guess:
-        incorrect_tries += 1
-        print("Incorrect guess. Letter not in word.")
-        print("Lives left:", 6 - incorrect_tries)
+        from wonderwords import RandomWord
+        word_generator = RandomWord()
+        word_to_guess = word_generator.word(word_min_length=6)
+        unique_letters = list(dict.fromkeys(word_to_guess, None))
+        revealed_letters = random.sample(population=unique_letters, k=len(word_to_guess)//4)
+
+      except subprocess.CalledProcessError:
+        print("Failed to install 'wonderwords'. Please install it manually.")
+        sys.exit(1)
     
     else:
-        guessed_list.append(guess)
-        print("Correct guess. Letter found in word.")
-    
-    hidden_word = ""
-    print()
+      mode = 2
 
-else:
-    print("You lost. The word was:", word_to_guess)
+if mode == 2:
+  import os
+  def clear_shell():
+  # Clear the shell based on the operating system
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+  word_to_guess = input("Enter the word to be guessed by all (min. length = 5): ").lower()
+
+  if len(word_to_guess) < 5: #Minimum length not met
+    raise ValueError("You were supposed to input a word of length more than 5.")
+
+  revealed_letters = []
+  for _ in range(len(word_to_guess)//4):
+    reveal_letter = input("Reveal a letter: ").lower()
+    revealed_letters.append(reveal_letter)
+
+  clear_shell()
+
+guessed_letters = []
+hidden_word = ""
+print("Start guessing the word now.")
+wrong_tries = 0
+
+while wrong_tries < 6:
+  print(drawings[wrong_tries], '\n')
+  for letter in word_to_guess:
+    if letter in (revealed_letters + guessed_letters) or (not letter.isalpha()):
+      hidden_word += letter
+
+    else:
+      hidden_word += "_"
+  
+  print(" ".join(hidden_word)) #make the hidden word more readable with spacing
+
+  if "_" not in hidden_word:
+    print("You won. Congratulations.")
+    print("Incorrect tries:", wrong_tries, "\n")
+    break
+    
+  guess = input("Guess a letter of the word: ").lower()
+  if guess not in word_to_guess:
+    wrong_tries += 1
+    print("INCORRECT GUESS!", 6 - wrong_tries, "lives remaining.\n")
+    hidden_word = ""
+  
+  else:
+    guessed_letters.append(guess)
+    print("CORRECT GUESS, LETTER FOUND IN WORD.\n")
+    hidden_word = ""
+
+else: #if the user loses, the while loop does NOT break, and reaches the ELSE block
+  print("You lost. The word was:", word_to_guess)
+  print(drawings[6], '\n')
